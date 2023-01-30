66a09df8c7f9a2e00ae687d42ff0bd8d122c4753
Justin Thoms <DyneXvX@outlook.com>
2023-01-30 11:09:59 -0500

    changed verticescount to 8

--- Dijkstra/Program.cs
+++ Dijkstra/Program.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Diagnostics;
-
-namespace DijkstraAlgorithm
+﻿namespace DijkstraAlgorithm
 {
     public static class Dijkstra
     {
--- DijkstraTests/DijkstraTests.cs
+++ DijkstraTests/DijkstraTests.cs
@@ -6,58 +6,31 @@ namespace DijkstraAlgorithm.Tests
     [TestClass()]
     public class DijkstraTests
     {
-        //[TestMethod()]
-        //public void DijkstraAlgoTest()
-        //{
-        //    //arrange
-        //    int[,] graph =  {
-        //                      { 0, 6, 0, 0, 0, 0, 0, 9, 0 },
-        //                      { 6, 0, 9, 0, 0, 0, 0, 11, 0 },
-        //                      { 0, 9, 0, 5, 0, 6, 0, 0, 2 },
-        //                      { 0, 0, 5, 0, 9, 16, 0, 0, 0 },
-        //                      { 0, 0, 0, 9, 0, 10, 0, 0, 0 },
-        //                      { 0, 0, 6, 0, 10, 0, 2, 0, 0 },
-        //                      { 0, 0, 0, 16, 0, 2, 0, 1, 6 },
-        //                      { 9, 11, 0, 0, 0, 0, 1, 0, 5 },
-        //                      { 0, 0, 2, 0, 0, 0, 6, 5, 0 }
-        //                        };
-
-        //    int source = 0;
-        //    int verticesCount = 9;
-        //    //act
-
-        //    Dijkstra.DijkstraAlgo(graph, source, verticesCount);
-
-        //    //assert
-
-        //    Assert.AreEqual(9, verticesCount);
-        //}
-
         [TestMethod()]
         public void DijkstraAlgo()
         {
             //arrange
 
-            int[,] graph = new int[,]  {
-                              { 0, 6, 0, 0, 0, 0, 0, 9, 0 },
-                              { 6, 0, 9, 0, 0, 0, 0, 11, 0 },
-                              { 0, 9, 0, 5, 0, 6, 0, 0, 2 },
-                              { 0, 0, 5, 0, 9, 16, 0, 0, 0 },
-                              { 0, 0, 0, 9, 0, 10, 0, 0, 0 },
-                              { 0, 0, 6, 0, 10, 0, 2, 0, 0 },
-                              { 0, 0, 0, 16, 0, 2, 0, 1, 6 },
-                              { 9, 11, 0, 0, 0, 0, 1, 0, 5 },
-                              { 0, 0, 2, 0, 0, 0, 6, 5, 0 }
+            int[,] graph = {
+                              { 0, 6, 0, 0, 0, 0, 0, 9, },
+                              { 6, 0, 9, 0, 0, 0, 0, 11, },
+                              { 0, 9, 0, 5, 0, 6, 0, 0, },
+                              { 0, 0, 5, 0, 9, 16, 0, 0, },
+                              { 0, 0, 0, 9, 0, 10, 0, 0, },
+                              { 0, 0, 6, 0, 10, 0, 2, 0, },
+                              { 0, 0, 0, 16, 0, 2, 0, 1, },
+                              { 9, 11, 0, 0, 0, 0, 1, 0, },
+                              { 0, 0, 2, 0, 0, 0, 6, 5, }
                                 };
 
             int source = 0;
-            int verticesCount = 9;
+            int verticesCount = 8;
 
             //act
             Dijkstra.DijkstraAlgo(graph, source, verticesCount);
 
             //assert
-            Assert.AreEqual(9, verticesCount);
+            Assert.AreEqual(8, verticesCount);
         }
     }
 }
